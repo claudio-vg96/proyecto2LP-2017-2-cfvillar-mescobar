61a3a48fbae63b36219333c5bb4c9a9a54eb96dc
claudio-vg96 <claudio.uc96@gmail.com>
2017-11-16 11:30:12 -0300

    cambios paquete equipamiento, se genera clase abstracta equipamiento y se comienza a implementar en armas y armaduras

--- src/equipamento/Armaduras.java
+++ src/equipamento/Armaduras.java
@@ -1,4 +1,12 @@
 package equipamento;
 
-public class Armaduras implements Equipamento {
+public class Armaduras extends Equipamento {
+    private int resistencia;
+    public void setResistencia(int resistencia){
+        this.resistencia = resistencia + super.getNivel()*2;
+    }
+
+    public int getResistencia(){
+        return resistencia;
+    }
 }
--- src/equipamento/Armas.java
+++ src/equipamento/Armas.java
@@ -1,4 +1,22 @@
 package equipamento;
 
-public class Armas implements Equipamento {
+public class Armas extends Equipamento {
+    private int danio;
+
+    public Armas(){
+
+    }
+
+    public Armas(int level){
+        super.setNivel(level);
+        danio = 10 + level;
+    }
+
+    public void setDaño(int danio){
+        this.danio = danio + super.getNivel()*5;
+    }
+
+    public int getDaño(){
+        return danio;
+    }
 }
